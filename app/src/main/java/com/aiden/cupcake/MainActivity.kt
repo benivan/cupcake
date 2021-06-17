e72ba87d89bb673edb0c25fd80e2ce96c06c692a
Vikash Kumar <itsmevikash09@gmail.com>
2021-06-17 12:59:00 +0530

    mainactivity

--- app/src/main/java/com/aiden/cupcake/MainActivity.kt
+++ app/src/main/java/com/aiden/cupcake/MainActivity.kt
@@ -2,10 +2,30 @@ package com.aiden.cupcake
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupActionBarWithNavController
+import com.aiden.cupcake.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.nav_controller_view_tag) as NavHostFragment
+
+        var navController = navHostFragment.navController
+
+        setupActionBarWithNavController(navController)
+    }
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 }
