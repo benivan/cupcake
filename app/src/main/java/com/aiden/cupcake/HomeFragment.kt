e49dd029fd6804499fad18621ee886feefa9cc2b
Vikash Kumar <itsmevikash09@gmail.com>
2021-06-18 00:13:20 +0530

    home view

---
+++ app/src/main/java/com/aiden/cupcake/HomeFragment.kt
@@ -0,0 +1,43 @@
+package com.aiden.cupcake
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.core.content.res.ResourcesCompat
+import com.aiden.cupcake.databinding.FragmentHomeBinding
+
+
+class HomeFragment : Fragment() {
+
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
+
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        super.onCreateView(inflater, container, savedInstanceState)
+         _binding = FragmentHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.mainLayout.setBackgroundColor(
+            ResourcesCompat.getColor(
+                resources,
+                R.color.teal_200,
+                null
+            )
+        )
+
+    }
+}
